895a80e96eadd96ab62717d672cd4762d344f68b
Lindanzavi <lindanzavi@gmail.com>
2024-09-17 05:25:22 -0700

    0-the_sky_is_the_limit_not.pp

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,6 +1,13 @@
-# change values in the file /etc/default/nginx/
+# Increases the amount of traffic an Nginx server can handle.
 
-exec {'concurrence_conections':
-  command  => "/usr/bin/sed -i 's/15/1000' /etc/default/nginx && sudo service nginx restart",
-  provider => shell,
+# Increase the ULIMIT of the default file
+exec { 'fix--for-nginx':
+  command => 'sed -i "s/15/4096/" /etc/default/nginx',
+  path    => '/usr/local/bin/:/bin/'
+} ->
+
+# Restart Nginx
+exec { 'nginx-restart':
+  command => 'nginx restart',
+  path    => '/etc/init.d/'
 }
